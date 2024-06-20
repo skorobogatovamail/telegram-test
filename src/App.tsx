3f8e7c48c3b771fd0b3216d8fdbb14d40b8a5e9c
skorobogatovamail <skorobogatovamail@gmail.com>
2024-06-20 12:25:09 +0300

    only client

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 const tg = window.Telegram.WebApp;
 
 function App(): JSX.Element {
   const onClose = () => {
     tg.close();
   };
+
   return (
     <div className="App">
       Work{' '}
